59ea8a6d27c093c59da77d5defa7bb03760822d1
Punched Studio <dmipin02@gmail.com>
2020-01-18 22:42:27 +0200

    Summary

--- bootloader/src/boot.c
+++ bootloader/src/boot.c
@@ -1,4 +1,11 @@
+#include <systypes.h>
+#include <mtktypes.h>
+
+extern uint32_t ROM_Image_Base, ROM_Image_Limit;
+
 int main(void)
 {
+    pSF_HEADER_v1 sf_header = (pSF_HEADER_v1)ROM_Image_Base;
+
     return 0;
 }
