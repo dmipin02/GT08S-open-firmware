f521540cbf6001478c2e912eaa7d32c7ff2d2cb0
ONROUNDIT <kartoxachanell@gmail.com>
2020-01-18 23:23:34 +0200

    sha1

---
+++ bootloader/include/sha1.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define SHA1BLOCKSIZE 64
+
+typedef struct
+{
+    uint32_t H0;
+    uint32_t H1;
+    uint32_t H2;
+    uint32_t H3;
+    uint32_t H4;
+} TSHA1, *pSHA1;
+
+extern pSHA1 SHA1_ProcessData(uint8_t *Data, uint32_t Length);
