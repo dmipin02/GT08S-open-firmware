c0224cc2b6d4167bb99d16cbdc940d36ca3ddaea
Punched Studio <dmipin02@gmail.com>
2020-01-18 23:02:56 +0200

    Kernel header

---
+++ kernel/src/kernelheader.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <mtktypes.h>
+
+typedef struct tag_APP_HEADER
+{
+    FILE_INFO_v1    file_info;
+    GFH_BL_INFO_v1  bl_info;
+} APP_HEADER;
+
+extern uint32_t __app_header_base;
+extern uint32_t __app_header_size;
+
+volatile const APP_HEADER app_header __attribute__ ((section (".app_header"))) =
+{
+    {
+        {GFH_HDR_MAGIC, sizeof(FILE_INFO_v1), GFH_FILE_INFO},
+        "FILE_INFO",
+        1,
+        ARM_EXT_BL,
+        F_SF,
+        1,
+        (uint32_t)(&__app_header_base),
+        0xffffffff,
+        0xffffffff,
+        (uint32_t)(&__app_header_size),
+        0x00000020,
+        (uint32_t)(&__app_header_size),
+        3
+    },
+    {
+        {GFH_HDR_MAGIC, sizeof(GFH_BL_INFO_v1), GFH_BL_INFO},
+        0
+    }
+};
