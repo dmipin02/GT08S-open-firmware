9eceea618b63d4b9f6d30b378a0416ad6f6821b3
Punched Studio <dmipin02@gmail.com>
2020-01-27 01:34:31 +0200

    Correct kernel initialization
    Finally it works!!!

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,4 +1,42 @@
+#include <drivers/vibro.h>
+#include <drivers/lcd.h>
+#include <drivers/pctl.h>
+#include <drivers/pll.h>
+#include <drivers/ustimer.h>
+#include <drivers/emi.h>
+#include <drivers/gpio.h>
+#include <drivers/rtc.h>
+#include <drivers/memory.h>
+#include <drivers/nvic.h>
+#include <drivers/lrtimer.h>
+
+#define _DEBUG_ (1)
+#define _USEBATTERY_ (0)
+
 int main(void)
 {
-    return 0;
+    MPU_Initialize();
+    PCTL_Initialize();
+    GPIO_Initialize();
+    RTC_Initialize();
+
+    USC_StartCounter();
+
+    size_t MemSize = InitializeMemoryPool();
+
+    EM_Initialize();
+    NVIC_Initialize();
+    LRT_Initialize();
+
+    EnableInterrupts();
+
+    // Signal about turning on the device
+    HW_Vibro_Toggle(false);
+    HW_LCD_SetISINKMode(false);
+    HW_LCD_SetISINKParameters(ISINK_CH0, IC_8mA, true);
+
+    while (1)
+    {
+        EM_ProcessEvents();
+    }
 }
